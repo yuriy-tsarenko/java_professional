276f97301f8717f75d3627f1144f178ae08b2a6e
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2021-03-12 22:05:09 +0200

    DEVELOPMENT-5: added classwork2 examples

---
+++ src/main/java/com/java_professional/tsarenko/classwork/lesson2/ex_004_set/Bike.java
@@ -0,0 +1,16 @@
+package com.java_professional.tsarenko.classwork.lesson2.ex_004_set;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+public class Bike {
+
+    private String brand;
+    private Integer speed;
+    private Integer maxDistance;
+}
