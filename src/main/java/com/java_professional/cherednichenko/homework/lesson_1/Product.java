16d14d8c1df93b5005c2720117aa325123f9d189
Nikita <4ered29nik@gmail.com>
2021-03-16 20:05:40 +0200

    fixed some mistakes

--- src/main/java/com/java_professional/cherednichenko/homework/lesson_1/Product.java
+++ src/main/java/com/java_professional/cherednichenko/homework/lesson_1/Product.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
+
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
@@ -26,19 +27,21 @@ public class Product {
     private int quantity;
 
     public static void main(String[] args) {
-        Comparator<Product> comparator = new ProductQuantityComparator().thenComparing(new ProductNameComparator().
-                thenComparing(new ProductTypeComparator()).thenComparing(new ProductPriceComparator()));
+        Comparator<Product> comparator = new ProductNameComparator();
         TreeSet<Product> products = new TreeSet<Product>(comparator);
         products.add(new Product("nameOfProduct1", "Type1", "SN123456", 1000, 0));
         products.add(new Product("nameOfProduct2", "Type2", "SN123456", 1000, 1));
         products.add(new Product("nameOfProduct3", "Type3", "SN123456", 1000, 55));
-        products.add(new Product("nameOfProduct4", "Type4", "SN123456", 1000, 2));
+        products.add(new Product("nameOfProduct7", "Type4", "SN123456", 1000, 2));
         products.add(new Product("nameOfProduct5", "Type5", "SN123456", 1000, 7));
         products.add(new Product("nameOfProduct6", "Type6", "SN123456", 1000, 9));
-        products.add(new Product("nameOfProduct7", "Type7", "SN123456", 1000, 2));
+        products.add(new Product("nameOfProduct4", "Type7", "SN123456", 1000, 2));
         products.add(new Product("nameOfProduct8", "Type8", "SN123456", 1000, 3));
         products.add(new Product("nameOfProduct9", "Type9", "SN123456", 1000, 111));
         products.add(new Product("nameOfProduct10", "Type10", "SN123456", 1000, 22));
+        products.add(new Product("CnameOfProduct9", "Type10", "SN123456", 1000, 22));
+        products.add(new Product("BnameOfProduct10", "Type10", "SN123456", 1000, 22));
+        products.add(new Product("AnameOfProduct10", "Type10", "SN123456", 1000, 22));
         Iterator<Product> productIterator = products.iterator();
         while (productIterator.hasNext()) {
             System.out.println(productIterator.next());
@@ -49,28 +52,15 @@ public class Product {
     static class ProductNameComparator implements Comparator<Product> {
         @Override
         public int compare(Product o1, Product o2) {
-            return o1.getNameOfProduct().compareTo(o2.getNameOfProduct());
-        }
-    }
-
-    static class ProductTypeComparator implements Comparator<Product> {
-        @Override
-        public int compare(Product o1, Product o2) {
-            return o1.getTypeOfProduct().compareTo(o2.getTypeOfProduct());
-        }
-    }
-
-    static class ProductPriceComparator implements Comparator<Product> {
-        @Override
-        public int compare(Product o1, Product o2) {
-            return Integer.compare(o1.getPrice(), o2.getPrice());
-        }
-    }
-
-    static class ProductQuantityComparator implements Comparator<Product> {
-        @Override
-        public int compare(Product o1, Product o2) {
-            return Integer.compare(o1.getQuantity(), o2.getQuantity());
+            int result;
+            result = Integer.compare(o1.getQuantity(), o2.getQuantity());
+            if (result == 0)
+                result = o1.getNameOfProduct().compareTo(o2.getNameOfProduct());
+            if (result == 0)
+                result = o1.getTypeOfProduct().compareTo(o2.getTypeOfProduct());
+            if (result == 0)
+                result = Integer.compare(o1.getPrice(), o2.getPrice());
+            return result;
         }
     }
 }
