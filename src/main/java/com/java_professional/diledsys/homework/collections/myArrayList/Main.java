3f902fe2591cfcd61a7da45d141bc7ac1937e90d
diledsys <diledsys@gmil.com>
2021-03-20 09:33:22 +0300

    HOMEWORK-4:ArrayList and LinkedList

---
+++ src/main/java/com/java_professional/diledsys/homework/collections/myArrayList/Main.java
@@ -0,0 +1,56 @@
+package com.java_professional.diledsys.homework.collections.myArrayList;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyLinkedList<String> myList = new MyLinkedList<>();
+        //linkedList.add("2");
+        //MyList<String> myList = new  MyArrayList();
+        myList.addFirst("Si");
+        myList.addFirst("Hr");
+        myList.addFirst("Fe");
+        myList.addFirst("Mg");
+        myList.addFirst("Cu");
+        myList.addFirst("Cu");
+        myList.addFirst("Cu2");
+        myList.addFirst("Cu");
+        myList.addFirst("Cu3");
+        myList.addFirst("err");
+
+
+        //System.out.println(myList.get(5));
+        System.out.println();
+        System.out.println(myList.removeLast());
+        for (int i = 0; i <myList.size() ; i++) {
+            System.out.println(myList.get(i));
+        }
+//        for (int i = 0; i <myList.size() ; i++) {
+//            System.out.println(myList.get(i));
+//        }
+//        System.out.println(myList.removeFirst());
+
+
+//            for (String str:myList)
+//                System.out.println("->"+str);
+//        System.out.println(Arrays.toString(myList.toArray()));
+//
+//        System.out.println(myList.size());
+//
+//        System.out.println(myList.remove("Cu3"));
+//
+//        System.out.println(myList.size());
+//
+//        System.out.println("mylist.lastIndexOf(\"Cu\")= " + myList.lastIndexOf("Cu"));
+//
+//        System.out.println("indexOf(\"Mg\") = " + myList.indexOf("Mg"));
+//
+//        System.out.println(myList.size());
+
+    }
+
+}
---
+++ src/main/java/com/java_professional/diledsys/homework/collections/myArrayList/MyDeque.java
@@ -0,0 +1,8 @@
+package com.java_professional.diledsys.homework.collections.myArrayList;
+
+public interface MyDeque<E> {
+
+    void addFirst(E e);
+
+    void addLast(E e);
+}
