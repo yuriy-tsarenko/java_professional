6e61f65d38e69cd2e2b2498a41d5fc43d5e0f5fc
Nikita <4ered29nik@gmail.com>
2021-03-22 22:32:48 +0200

    HOMEWORK-2: Fixed bugs from CI

--- src/main/java/com/java_professional/cherednichenko/homework/lesson_5/CutString.java
+++ src/main/java/com/java_professional/cherednichenko/homework/lesson_5/CutString.java
@@ -5,11 +5,11 @@ package com.java_professional.cherednichenko.homework.lesson_5;
 //        Разбить эту строку на 2 подстроки равной длине и вывести на экран каждое предложения с новой строки.
 public class CutString {
     public static void main(String[] args) {
-        String text ="Необходимо создать строку с текстом (текст взять любой из интернета). " +
-                "Разбить эту строку на 2 подстроки равной длине и вывести на экран каждое предложения с новой строки.";
-        int half = text.length()/2;
+        String text = "Необходимо создать строку с текстом (текст взять любой из интернета). "
+                + "Разбить эту строку на 2 подстроки равной длине и вывести на экран каждое предложения с новой строки.";
+        int half = text.length() / 2;
         System.out.println(half);
-        System.out.println(text.substring(0,half));
+        System.out.println(text.substring(0, half));
         System.out.println(text.substring(half));
     }
 }
