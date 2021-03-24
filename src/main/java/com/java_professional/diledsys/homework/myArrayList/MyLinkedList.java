fde78add855c6b6e582946afcc6ae5f0d9e9a310
diledsys <diledsys@gmil.com>
2021-03-24 23:14:14 +0300

    Homework-1:collections_arrayList_linkedList

---
+++ src/main/java/com/java_professional/diledsys/homework/myArrayList/MyLinkedList.java
@@ -0,0 +1,98 @@
+package diledsys.homework.myArrayList;
+
+import java.util.Spliterator;
+
+public class MyLinkedList<E> extends MyArrayList implements MyDeque {
+    private Node head;
+    private Node last;
+
+    private int size;
+
+    @Override
+    public void add(Object o) {
+        addFirst(o);
+    }
+
+    @Override
+    public void addFirst(Object o) {
+        Node node = new Node();
+        node.element = o;
+        if (head != null) {
+            node.next = head;
+        }
+        head = node;
+        size++;
+    }
+
+    @Override
+    public void addLast(Object o) {
+        Node node = new Node();
+        node.element = o;
+        if (last != null) {
+            node.next = last;
+        }
+        last = node;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Object remove(Object o) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head == last) {
+            head = null;
+            last = null;
+            return null;
+        }
+
+        if (head.element == o) {
+            head = head.next;
+            return head.element;
+        }
+
+        Node t = head;
+        while (t.next != null) {
+            if (t.next.element == o) {
+                if (last == t.next) {
+                    last = t;
+                }
+                t.next = t.next.next;
+                return t.element;
+            }
+            t = t.next;
+        }
+        return null;
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
+    }
+
+    @Override
+    public Object get(int index) {
+        return find(index);
+    }
+
+    private Object find(int index) {
+        Node t = head;
+        Object o = null;
+        for (int i = 0; i < index; i++) {
+            o = t.element;
+            t = t.next;
+        }
+        return o;
+    }
+
+    class Node<E> {
+        E element;
+        Node next;
+    }
+}
