6ec58b424745cebf052dedf980fda6d700daaef5
Yuriy Tsarenko <ytsarenko@conceptsoft.io>
2021-03-27 00:18:03 +0200

    DEVELOPMENT-13: added lesson8 examples

--- src/main/java/javaProfessional/ex_001_Annotations/methods/AnotherDog.java
+++
@@ -1,6 +0,0 @@
-package javaProfessional.ex_001_Annotations.methods;
-
-import com.java_professional.tsarenko.classwork.lesson7.ex_001_Annotations.methods.Dog;
-
-public class AnotherDog extends Dog {
-}
