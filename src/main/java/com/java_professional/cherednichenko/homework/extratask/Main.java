67646bae57fac5ebc0b856cb8ff4e4b08e02889a
Nikita <4ered29nik@gmail.com>
2021-04-02 16:10:20 +0300

    HOMEWORK-13: fix CI

--- src/main/java/com/java_professional/cherednichenko/homework/extratask/Main.java
+++ src/main/java/com/java_professional/cherednichenko/homework/extratask/Main.java
@@ -1,6 +1,10 @@
 package com.java_professional.cherednichenko.homework.extratask;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
