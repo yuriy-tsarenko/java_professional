3591fe949e62832b7cceb9d4c3e22bc0df60500a
Nikita <4ered29nik@gmail.com>
2021-04-02 21:17:52 +0300

    HOMEWORK-14: added sort()

--- src/main/java/com/java_professional/cherednichenko/homework/extratask/Main.java
+++ src/main/java/com/java_professional/cherednichenko/homework/extratask/Main.java
@@ -1,18 +1,20 @@
 package com.java_professional.cherednichenko.homework.extratask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] integers = new Integer[]{1, 2, 3, 6, 7};
-        System.out.println(stringBuilder(integers));
+        Integer[] integers = new Integer[]{1, 2, 1, 2, 5, 6, 7, 4, 6};
+        Arrays.sort(integers);
+        System.out.println(stringBuilder(sort(integers)));
         String[] strings = new String[]{"Monday", "Wednesday", "Friday", "Saturday", "Sunday"};
-        System.out.println(stringBuilder(getIntegers(strings)));
-
+        System.out.println(stringBuilder(sort(getIntegers(strings))));
     }
 
     public static StringBuilder stringBuilder(Integer[] integers) {
@@ -27,7 +29,7 @@ public class Main {
             } else if (i + 1 == integers.length) {
                 string.append("-");
                 string.append(hashMap.get(integers[i]));
-            } else if ((integers[i] - integers[i - 1] == 1 & integers[i + 1] - integers[i] != 1)) {
+            } else if ((integers[i] - integers[i - 1] >= 1 & integers[i + 1] - integers[i] > 1)) {
                 string.append("-");
                 string.append(hashMap.get(integers[i]));
             }
@@ -48,5 +50,11 @@ public class Main {
         }
         Integer[] ints = new Integer[integers.size()];
         return integers.toArray(ints);
+    }
+
+    public static Integer[] sort(Integer[] integers) {
+        Set<Integer> set = new HashSet<>(Arrays.asList(integers));
+        integers = set.toArray(new Integer[set.size()]);
+        return integers;
     }
 }
